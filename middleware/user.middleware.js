4e567f7605f35bde01a264e0995a7d966aa89e4c
TheDebugger001 <thedebugger001@gmail.com>
2026-02-04 13:27:06 +0200

    back to it

--- middleware/user.middleware.js
+++ middleware/user.middleware.js
@@ -1,3 +1,36 @@
+const express = express();
+const bcrypt = require('bcryptjs')
+const jwt = require("jsonwebtoken");
+const User = required("../models/users.models.js");
+const { body, validationResults } = require("express-validator");
+const User = require("../models/users.models");
 
-const jwt = require("jsonwebtoken")
-const { body, validationResults } = require("express-validator")
+exports.Register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password)
+      return res.status(401).json({ message: "All fields are required" });
+    const user_exists = await User.findOne({ email });
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    if (user_exists)
+      return res
+        .status(401)
+        .json({ message: "email already used in registering" });
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword
+  })
+
+  res.status(201).json({
+    name: user.name,
+    email: user.email,
+    role: 'User'
+  })
+  } catch (error) {
+    res.status(500).json({ error: "Server error", error})
+  }
+};
--- models/users.models.js
+++ models/users.models.js
@@ -2,12 +2,13 @@
 const mongoose = require("mongoose")
 
 const UserModel = new mongoose.Schema({
-  names : { type:String, required:true },
+  names : { type:String, required:true, unique:true},
   password : {
     type: String,
-    length: 4, 
+    length: 4,
     required: true},
   email: {type:email, required: true},
+  role: {type:String, required:true , enum:['user', 'librarian', 'admin'], default:'user'}
 }, {timestamps: true})
 
 const User = mongoose.model("Users", UserModel)
