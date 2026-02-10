98baa8935700c60c6798b26fb20be71f73ae2a31
TheDebugger001 <thedebugger001@gmail.com>
2026-02-10 10:13:38 +0200

    Validation of registering inputs

--- middleware/user.middleware.js
+++ middleware/user.middleware.js
@@ -2,5 +2,15 @@ const express = require("express");
 const { body, param } = require("express-validator");
 
 exports.registerValidation = [
-  body()
+  body("names")
+    .notEmpty().withMessage("Names should be provided")
+    .isString().withMessage("Names should be String")
+    .isLength({ min: 3 }).withMessage("Names should have at least 3 characters"),
+  body("email")
+    .notEmpty().withMessage("Email should be provided")
+    .isString().withMessage("Email should be String")
+    .isLength({ min: 3 }).withMessage("Email should have at least 3 characters"),
+  body("password")
+    .notEmpty().withMessage("Password should be provided")
+    .isLength({ min: 6 }).withMessage("Password should have at least 6 characters"),
 ]
