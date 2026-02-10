98ef145016990d227b79ebae71010a5578988d98
TheDebugger001 <thedebugger001@gmail.com>
2026-02-10 18:31:05 +0200

    Creation of Authentication

--- middleware/user.middleware.js
+++ middleware/user.middleware.js
@@ -1,16 +1,44 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const { body, param } = require("express-validator");
+require("dotenv").config()
 
 exports.registerValidation = [
   body("names")
-    .notEmpty().withMessage("Names should be provided")
-    .isString().withMessage("Names should be String")
-    .isLength({ min: 3 }).withMessage("Names should have at least 3 characters"),
+    .notEmpty()
+    .withMessage("Names should be provided")
+    .isString()
+    .withMessage("Names should be String")
+    .isLength({ min: 3 })
+    .withMessage("Names should have at least 3 characters"),
   body("email")
-    .notEmpty().withMessage("Email should be provided")
-    .isString().withMessage("Email should be String")
-    .isLength({ min: 3 }).withMessage("Email should have at least 3 characters"),
+    .notEmpty()
+    .withMessage("Email should be provided")
+    .isString()
+    .withMessage("Email should be String")
+    .isLength({ min: 3 })
+    .withMessage("Email should have at least 3 characters"),
   body("password")
-    .notEmpty().withMessage("Password should be provided")
-    .isLength({ min: 6 }).withMessage("Password should have at least 6 characters"),
-]
+    .notEmpty()
+    .withMessage("Password should be provided")
+    .isLength({ min: 6 })
+    .withMessage("Password should have at least 6 characters"),
+];
+
+module.exports = (req, res, next) => {
+  try {
+    const authHeader = req.header("Authorization");
+
+    if (!authHeader)
+      return res
+        .status(401)
+        .json({ message: "Denied access, Unauthorized request" });
+
+    const token = authHeader.split(" ")[1];
+
+    const decode = jwt.verify(token, process.env.JWT_SECRET)
+    req.user = decode
+  } catch (error) {
+    return res.status(501).json({ message: "Internal Server error" });
+  }
+};
