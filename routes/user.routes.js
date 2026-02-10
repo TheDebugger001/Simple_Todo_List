d69da2128b5d5096b505c021d335a4a3acca08ef
TheDebugger001 <thedebugger001@gmail.com>
2026-02-10 10:20:32 +0200

    Finisheed register routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const { register, login } = require("../controllers/authControllers")
+const { registerValidation } = require("../middleware/user.middleware")
 
 const router = express.Router()
 
-router.post("/register/", register)
-router.post("/login/", login)
+router.post("/register/", registerValidation, register)
+router.post("/login/", registerValidation, login)
