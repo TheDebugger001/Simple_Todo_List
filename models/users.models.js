f050b822d00effa81b08ca7710522127ba31b14a
TheDebugger001 <thedebugger001@gmail.com>
2026-02-12 11:41:29 +0200

    Creation of Todo

--- models/users.models.js
+++ models/users.models.js
@@ -4,8 +4,9 @@ const userSchema = new mongoose.Schema({
   names: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: {type: String, enum: ['user', 'admin'], default: 'user'},
+  role: { type: String, enum: ["user", "admin"], default: "user" },
   createdAt: { type: Date, default: Date.now },
+  refreshToken: { type: String },
 });
 
-module.exports = mongoose.model("User", userSchema)
+module.exports = mongoose.model("User", userSchema);
